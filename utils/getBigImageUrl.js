5ff124d8fa241b9427efaef9bc0daae17a1e3a57
[sk developer] <[skdeveloper101@gmail.com]>
2023-05-29 11:28:48 +0530

    fix deployment error.

--- utils/getBigImageUrl.js
+++ utils/getBigImageUrl.js
@@ -1,14 +1,18 @@
 export const getBigImageUrl = (url) => {
-  const segments = url.split(".");
+  try {
+    const segments = url?.split(".");
 
-  segments[2] += "-w480_h360_x2";
+    segments[2] += "-w480_h360_x2";
 
-  let first = segments[0];
-  let second = segments[1];
-  let third = segments[2];
-  let fourth = segments[3];
+    let first = segments[0];
+    let second = segments[1];
+    let third = segments[2];
+    let fourth = segments[3];
 
-  let newUrl = first + "." + second + "." + third + "." + fourth;
+    let newUrl = first + "." + second + "." + third + "." + fourth;
 
-  return newUrl;
+    return newUrl;
+  } catch (error) {
+    console.log(error);
+  }
 };
--- utils/getCardUrl.js
+++ utils/getCardUrl.js
@@ -1,14 +1,18 @@
 export const getCardUrl = (url) => {
-  const segments = url.split(".");
+  try {
+    const segments = url?.split(".");
 
-  segments[2] += "-w480_h360";
+    segments[2] += "-w480_h360";
 
-  let first = segments[0];
-  let second = segments[1];
-  let third = segments[2];
-  let fourth = segments[3];
+    let first = segments[0];
+    let second = segments[1];
+    let third = segments[2];
+    let fourth = segments[3];
 
-  let newUrl = first + "." + second + "." + third + "." + fourth;
+    let newUrl = first + "." + second + "." + third + "." + fourth;
 
-  return newUrl;
+    return newUrl;
+  } catch (error) {
+    console.log(error);
+  }
 };
