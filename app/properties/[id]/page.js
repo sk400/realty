b3370fd5b8cb7b3583771c1b3f68bc8fe8ee5d84
[sk developer] <[skdeveloper101@gmail.com]>
2023-05-29 11:18:22 +0530

    Add isr support in property details page

--- app/properties/[id]/page.js
+++ app/properties/[id]/page.js
@@ -1,6 +1,17 @@
 import { PropertyDetailsPageComponents } from "@/app/(components)";
+import { fetchProperties } from "@/utils/fetchProperties";
 import { fetchPropertyDetails } from "@/utils/fetchPropertyDetails";
 import React from "react";
+
+export async function generateStaticParams() {
+  const data = await fetchProperties(50);
+
+  const properties = data?.data?.home_search?.results;
+
+  return properties.map((property) => ({
+    id: property.property_id,
+  }));
+}
 
 const PropertyDetail = async ({ params }) => {
   const data = await fetchPropertyDetails(params?.id);
